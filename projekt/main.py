986c66070c79e30f216fabada4b33ca79cd5610a
Liber151 <37102671+Liber151@users.noreply.github.com>
2018-05-30 17:08:49 +0200

    clean up
    Usuniecie niepotrzebnych rzeczy i oczyszczenie widoku uzytkownika, wstep do projektowania hit boxow

--- projekt/main.py
+++ projekt/main.py
@@ -5,37 +5,30 @@ import time
 import math
 def nothing(x):
     pass
-start=time.time()
-lefthand=[300,0];
+
+    
 timer=0
 cam = cv2.VideoCapture(0)
-
 cv2.namedWindow("test")
 fgbg = cv2.createBackgroundSubtractorMOG2()
 counter=0
 img_counter = 0
 img=np.zeros((300,512,3),np.uint8)
-cv2.createTrackbar('Ilosc punktow','test',1,500,nothing)
-cv2.createTrackbar('Odleglosc pomiedzy punktami','test',1,255,nothing)
+cv2.createTrackbar('Ilosc punktow','test',100,500,nothing)
+cv2.createTrackbar('Odleglosc pomiedzy punktami','test',10,255,nothing)
 cv2.createTrackbar('Czulosc','test',2,10,nothing)
 dystans=30
 plt.show()
-print("Rozpoczecie kalibracji")
 while True:
-
-
-    if (time.time()-start>6 and timer==0):
-        print("koniec kalibracji")
-        timer=1
-        for i in corners:
-            x,y = i.ravel()
-            if x<lefthand[0]:
-                lefthand=[x,y]
+    
     ret, frame = cam.read()
     frame=cv2.flip(frame, 1 )
+    
+    cv2.imshow("test", frame)
     frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     fgmask = fgbg.apply(frame)
-    cv2.imshow("test", fgmask)
+    
+    fgmask=cv2.flip(fgmask, 0 )
     punkty=cv2.getTrackbarPos('Ilosc punktow','test')
     silka=cv2.getTrackbarPos('Odleglosc pomiedzy punktami','test')
     czulkosc=cv2.getTrackbarPos('Czulosc','test')
@@ -48,23 +41,15 @@ while True:
     try:
         for i in corners:
             x,y = i.ravel()
-            if (timer==1):
-                temp=math.sqrt((lefthand[0]-x)**2+(lefthand[1]-y)**2)
-                if (temp<50 and temp<odleglosc):
-                   
-                    odleglosc=temp
-                    lefthand=[x,y]
             ax.append(x)
             ay.append(y)
-        plt.plot(ax,ay,'*','r')
-        plt.plot((600/2,lefthand[0]),(480/2,lefthand[1]))
-        plt.axis([0,600,480,0])
+        plt.plot(ax,ay,'*')
+        plt.axis([0,600,0,480])
         plt.draw()
         plt.pause(1e-17)
         plt.clf()
     except:
         pass
-    
     if not ret:
         break
     k = cv2.waitKey(1)
@@ -73,13 +58,6 @@ while True:
         # ESC pressed
         print("Escape hit, closing...")
         break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
-
+    
 cam.release()
-
 cv2.destroyAllWindows()
