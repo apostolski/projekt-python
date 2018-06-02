b02c862c1e07a6f62af22d16dc4aa2e905f641b1
Liber151 <37102671+Liber151@users.noreply.github.com>
2018-06-02 21:06:50 +0200

    Breakdown breakdown
    Naprawiono sprawdzanie hitboxa

--- projekt/main.py
+++ projekt/main.py
@@ -16,16 +16,32 @@ def znajdz_prostopadla(rownanie,x,y):
     prostopadla_do_prostej=[prostopadla_do_prostej,x[0]*prostopadla_do_prostej*(-1)+y[0]]
     return prostopadla_do_prostej
 class core_prosta:
-    def __init__(self,x,y,dol,lewo,prawo):
+    def __init__(self,x,y,dol):
         self.x=x
         self.y=y
         self.dol=dol
-        self.lewo=lewo
-        self.prawo=prawo
         self.rownanie=znajdz_rownanie(x,y)
         self.rownaniepp=znajdz_prostopadla(self.rownanie,x,y)
-        self.xplota=[x[0],x[1],x[1]+prawo*self.rownaniepp[0]*(-1)/2,x[0]+prawo*self.rownaniepp[0]*(-1)/2,x[0]]
+        self.prawo=((((y[0]-dol)-self.rownaniepp[1])/self.rownaniepp[0])-x[0])
+        self.xplota=[x[0],x[1],x[1]+self.prawo*self.rownaniepp[0]*(-1)/2,x[0]+self.prawo*self.rownaniepp[0]*(-1)/2,x[0]]
         self.yplota=[y[0],y[1],y[1]-dol,y[0]-dol,y[0]]
+def zplotuj(Obiekt):
+    plt.plot(Obiekt.xplota,Obiekt.yplota)
+def sprawdzhitboxa(Obiekt,x,y):
+    odleglosc_x=Obiekt.x[1]-Obiekt.x[0]
+    odleglosc_y=Obiekt.y[1]-Obiekt.y[0]
+    dlugosc_krzywej_x=math.sqrt(odleglosc_x**2+odleglosc_y**2)
+    dlugosc_krzywej_y=math.sqrt(Obiekt.dol**2+Obiekt.prawo**2)
+    wspolczynniky=dlugosc_krzywej_y/odleglosc_y
+    wspolczynnikx=dlugosc_krzywej_x/odleglosc_x
+    if ((y>(x*Obiekt.rownaniepp[0]+Obiekt.rownaniepp[1])and y<(x*Obiekt.rownanie[0]+Obiekt.rownanie[1])) and y>((x*Obiekt.rownanie[0]+Obiekt.rownanie[1])-(Obiekt.dol*wspolczynnikx)) and (y<(x*Obiekt.rownaniepp[0]+Obiekt.rownaniepp[1]+dlugosc_krzywej_x*Obiekt.rownaniepp[0]*(-1)))):
+        return True
+    else:
+        return False
+#class Figura:
+ #   def __init__(self,x,y,dol,prawo):
+##Wlozyc boxy w figury a figury w tablice
+
 timer=0
 cam = cv2.VideoCapture(0)
 cv2.namedWindow("test")
@@ -39,8 +55,9 @@ cv2.createTrackbar('Odleglosc pomiedzy punktami','test',10,255,nothing)
 cv2.createTrackbar('Czulosc','test',2,10,nothing)
 dystans=30
 plt.show()
-Proba=core_prosta([275,575],[296,497],100,20,100)
-print(Proba.rownaniepp)
+Proba=core_prosta([275,495],[296,417],50)
+
+
 while True:
     
     dab = cv2.resize(dab, (640, 480)) 
@@ -70,7 +87,9 @@ while True:
     ax=[]
     ay=[]
     dx=[]
-    dy=[]
+    dy=[]           #resetowanie wartosci wyswietlanych na wizualizacji danych
+    resztax=[]
+    resztay=[]
     odleglosc=30
     
     try:    #dla 1-szej klatki trzeba zrobic wyjatek bo skrypt usuwania tla potrzebuje bufora
@@ -79,13 +98,19 @@ while True:
             ax.append(x)
             ay.append(y)
             #
-            if (y>(x*Proba.rownaniepp[0]+Proba.rownaniepp[1])and y<(x*Proba.rownanie[0]+Proba.rownanie[1])):
+            #if (y>(x*Proba.rownaniepp[0]+Proba.rownaniepp[1])and y<(x*Proba.rownanie[0]+Proba.rownanie[1])):
+            if(sprawdzhitboxa(Proba,x,y)):
                 dx.append(x)
                 dy.append(y)
-
-        plt.plot(ax,ay,'*')
-        plt.plot(Proba.xplota,Proba.yplota)
+            else:
+                resztax.append(x)
+                resztay.append(y)
+                
+        #plt.plot(ax,ay,'*')
+        zplotuj(Proba)
+        #plt.plot(Proba.xplota,Proba.yplota)
         plt.plot(dx,dy,'*')
+        plt.plot(resztax,resztay,'*')
         plt.axis([0,600,0,480])
         plt.draw()
         plt.pause(1e-17)
